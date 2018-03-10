7ddbf65e7a7ddc5159ee2cb100260ee49ca6e87a
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-03-10 21:03:16 +0100

    operations with map

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-// create Map from an array
+const Immutable = window.Immutable;
 
-const data = [{1: 2},{3: 4}].map((item, index) => [index, item]);
+const data = {
+    user1: {
+        name: 'Andrei',
+        surname: 'Fidelman'
+    },
+    user2: {
+        name: 'Karina',
+        surname: 'Anisimova'
+    },
+    list: [
+        1, 2, 3
+    ]
+};
 
-// [
-//     [index, value],
-//     [index, value],
-// ]
+const map = Immutable.fromJS(data); // transform to Map/List the whole object
 
-const map = Immutable.Map(data);
-
-console.log(0, map.get(0)); // {1,2}
-console.log(1, map.get(1)); // {3,4}
+// get
+console.log('get name', map.getIn(['user1', 'name']));
+// set
+const mapSet = map.setIn(['user1', 'name'], 'Andy');
+console.log('map set', mapSet.toJS());
+// delete
+const mapDelete = map.delete('user1');
+console.log('map delete', mapDelete.toJS());
+// update
+const mapUpdate = map.update('list', list => list.map(item => item + '💩'));
+console.log('map update', mapUpdate.toJS());
+// clear
+const mapClear = map.clear();
+console.log('map clear', mapClear.toJS());
+// merge
+const additionalData = {
+    user3: {
+        name: 'Veronika',
+        surname: 'Kopchuk'
+    }
+};
+const mapMerge = map.merge(additionalData);
+console.log('map merge', mapMerge.toJS());
