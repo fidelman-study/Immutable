71cc54595a01ed6c802d2536d140957528717839
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-03-11 19:04:31 +0100

    querying map

--- index.js
+++ index.js
@@ -17,25 +17,18 @@ const data = {
 const map = Immutable.fromJS(data); // transform to Map/List the whole object
 
 // get
-console.log('get name', map.getIn(['user1', 'name']));
-// set
-const mapSet = map.setIn(['user1', 'name'], 'Andy');
-console.log('map set', mapSet.toJS());
-// delete
-const mapDelete = map.delete('user1');
-console.log('map delete', mapDelete.toJS());
-// update
-const mapUpdate = map.update('list', list => list.map(item => item + '💩'));
-console.log('map update', mapUpdate.toJS());
-// clear
-const mapClear = map.clear();
-console.log('map clear', mapClear.toJS());
-// merge
-const additionalData = {
-    user3: {
-        name: 'Veronika',
-        surname: 'Kopchuk'
-    }
-};
-const mapMerge = map.merge(additionalData);
-console.log('map merge', mapMerge.toJS());
+console.log('get', map.get('user1').toJS());
+// getIn
+console.log('get it', map.getIn(['user1', 'name']));
+// has
+console.log('has', map.has('list')); // keys
+// has in
+console.log('has in', map.hasIn(['user1', 'name']));
+// includes
+console.log('includes', map.get('list').includes(3));
+// first
+console.log('first', map.first().toJS());
+// last
+console.log('last', map.last().toJS());
+// find
+console.log('find', map.find(value => value.includes('Andrei')).toJS());
