627609945cb0b48a775409ccfeee1f8937579ada
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-03-12 22:05:51 +0100

    Reference comparison

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const Immutable = window.Immutable;
 
-const range = Immutable.Range(1);
+const set = Immutable.Set.of(1,2,2); // collection of unique items
 
-// Infinity range
-console.log('the range size', range.size);
+// check set
+console.log('set', set.toJS());
+
+// comparison
+const collection = [1,2,3];
+const list1 = Immutable.List(collection);
+const list2 = Immutable.List(collection);
+
+// plain non-equality
+console.log('plain comparison', list1 === list2);
+
+// hashCode comparison
+console.log('hashCode comparison', list1.hashCode() === list2.hashCode());
