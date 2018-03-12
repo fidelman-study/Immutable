319002e6756e3c5176be24a6166c96464c32ef67
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-03-12 20:42:06 +0100

    Range

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const Immutable = window.Immutable;
 
-// lazy array
-// order is not important
-// chain does not create intermediate collections
-const sequence = Immutable.Seq.of(1,2,3);
+const range = Immutable.Range(1);
 
-// sequence
-console.log('sequence', sequence.toJS());
-
-// map
-const seqMap = sequence.map(item => item * 2);
-console.log('seq map', seqMap.toJS())
-
-// take
-const taken2 = sequence.take(2);
-console.log('take 2', taken2.toArray());
+// Infinity range
+console.log('the range size', range.size);
